4dd7e6f50337d2f6e6bcdde6830d6cf1f096ebbb
Dan Galera <danerick.galera@gmail.com>
2020-11-20 10:02:59 +0800

    #8: integrated typescript,eslint,prettier, and path alias

---
+++ src/components/commons/Button.tsx
@@ -0,0 +1,8 @@
+const Button = () => {
+  return (
+    <>
+      <button type="button">Click Me</button>
+    </>
+  );
+};
+export default Button;
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import { Button } from "@numly/components";
+
+export default function Home() {
+  return (
+    <>
+    <p>Hello</p>
+    <Button/>
+    </>
+  )
+}
