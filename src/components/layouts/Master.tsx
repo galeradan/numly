c908e28aee7625c642c258b833aec6e6df343eda
Dan Galera <danerick.galera@gmail.com>
2020-12-20 19:07:46 +0800

    #6 wip: sidenav, master layout and styling

--- src/components/layouts/Master.tsx
+++ src/components/layouts/Master.tsx
@@ -13,25 +13,27 @@ const Master = ({ children }) => {
     <>
       <div className="master">
         <nav className="side-nav">
-          <span>Numly</span>
-          <ul>
-            <li className="nav-item">
-              <HomeIcon className="icon"/>
-              <span>Home</span>
-            </li>
-            <li className="nav-item">
-              <GenerateIcon className="icon"/>
-              Generate
-            </li>
-            <li className="nav-item">
-              <HistoryIcon className="icon"/>
-              History
-            </li>
-            <li className="nav-item">
-              <LogoutIcon className="icon"/>
-              Logout
-            </li>
-          </ul>
+          <div className="nav-content">
+            <p className="logo">Numly</p>
+            <ul className="nav-menu">
+              <li className="nav-item">
+                <HomeIcon className="icon"/>
+                <span>Home</span>
+              </li>
+              <li className="nav-item">
+                <GenerateIcon className="icon"/>
+                Generate
+              </li>
+              <li className="nav-item">
+                <HistoryIcon className="icon"/>
+                History
+              </li>
+              <li className="nav-item">
+                <LogoutIcon className="icon"/>
+                Logout
+              </li>
+            </ul>
+          </div>
         </nav>
         <main className="main">{children}</main>
       </div>
